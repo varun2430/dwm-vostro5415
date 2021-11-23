dd8148623510a57d0decae1cee8f5a8515184651
Varun Kadkade <varunkadkade2442@gmail.com>
2021-11-23 21:24:30 +0530

    changed autostart path

--- dwm.c
+++ dwm.c
@@ -1416,8 +1416,8 @@ run(void)
 
 void
 runautostart(void) {
-	system("cd ~/.config/dwm-vostro5415; ./autostart_blocking.sh");
-	system("cd ~/.config/dwm-vostro5415; ./autostart.sh &");
+	system("cd $HOME/.config/suckless/dwm-vostro5415; ./autostart_blocking.sh");
+	system("cd $HOME/.config/suckless/dwm-vostro5415; ./autostart.sh &");
 }
 
 void
